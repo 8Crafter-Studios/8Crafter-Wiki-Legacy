42ecdef154e28d50ea616be50033da3268633c78
8Crafter <134829412+Andexter8@users.noreply.github.com>
2025-03-07 10:54:28 -0800

    Create template-ImagePlaceholder.vue

---
+++ docs/andexdb/.vitepress/theme/components/content/template-ImagePlaceholder.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const props = defineProps<{
+  size?: number
+}>();
+</script>
+<template>
+  <image src="/assets/images/icons/missing_pack_icon.png" :width="props.size ?? 64" :height="props.size ?? 64" />
+</template>
